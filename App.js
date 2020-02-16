ac9121b87ea78a01d8b621c7be15b9c633a89983
Cristian Giordani <cristian@cuby.com.br>
2020-02-16 13:41:08 -0300

    1.0

--- App.js
+++ App.js
@@ -1,38 +1,87 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+/* eslint-disable prettier/prettier */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, { Fragment, Component } from 'react';
+import { SafeAreaView, View, Text, ActivityIndicator, StatusBar, StyleSheet } from 'react-native';
+import { LoginButton, AccessToken, GraphRequest, GraphRequestManager } from 'react-native-fbsdk';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends Component {
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <Text>Testando...</Text>
-      </SafeAreaView>
-    </>
-  );
-};
+  state = {
+    loading: false,
+    user: null,
+  };
 
-export default App;
+  getUserCallback = (error, result) => {
+    if (error) {
+      console.log('error', error);
+    } else {
+      this.setState({ user: result, loading: false });
+    }
+  }
+  getUserInfo = (token) => {
+    const infoRequest = new GraphRequest('/me', {
+      accessToken: token,
+      parameters: {
+        fields: {
+          string: 'email, name',
+        },
+      },
+    }, this.getUserCallback);
+
+    new GraphRequestManager().addRequest(infoRequest).start();
+  }
+  render() {
+    return (
+      <Fragment>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView style={styles.container}>
+
+          <LoginButton
+            permissions={['public_profile', 'email']}
+            onLoginFinished={async (error, result) => {
+              if (error) {
+                console.log('auth error', error);
+              } else if (result.isCancelled) {
+                console.log('isCancelled');
+              } else {
+                const accessData = await AccessToken.getCurrentAccessToken();
+                this.setState({ loading: true });
+                this.setState({ user: null });
+                this.getUserInfo(accessData.accessToken);
+              }
+            }}
+            onLogoutFinished={() => {
+              this.setState({ user: null });
+            }}
+          />
+          <View>
+            {this.state.loading && <ActivityIndicator />}
+            {this.state.user && (
+              <Fragment>
+                <Text style={styles.user}>{this.state.user.name}</Text>
+                <Text style={styles.email}>{this.state.user.email}</Text>
+              </Fragment>
+            )}
+          </View>
+
+        </SafeAreaView>
+      </Fragment>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  user: {
+    fontWeight: 'bold',
+    paddingTop: 20,
+  },
+  email: {
+    fontSize: 12,
+  },
+});
--- ios/authFacebook/AppDelegate.m
+++ ios/authFacebook/AppDelegate.m
@@ -11,14 +11,27 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+/* ADICIONADO */
+/* ADICIONADO */
+  #import <FBSDKCoreKit/FBSDKCoreKit.h>
+/* ADICIONADO */
+/* ADICIONADO */
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"authFacebook"
-                                            initialProperties:nil];
+    moduleName:@"authFacebook"
+    initialProperties:nil];
+    
+  /* ADICIONADO */
+  /* ADICIONADO */
+    [[FBSDKApplicationDelegate sharedInstance] application:application 
+    didFinishLaunchingWithOptions:launchOptions];
+  /* ADICIONADO */
+  /* ADICIONADO */
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
@@ -30,6 +43,24 @@
   return YES;
 }
 
+
+/* ADICIONADO */
+/* ADICIONADO */
+  - (BOOL)application:(UIApplication *)application 
+      openURL:(NSURL *)url 
+      options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options { 
+  BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application 
+  openURL:url 
+  sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey] 
+  annotation:options[UIApplicationOpenURLOptionsAnnotationKey] 
+  ]; 
+  // Add any custom logic here. 
+  return handled; 
+  }
+/* ADICIONADO */
+/* ADICIONADO */
+
+
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
@@ -40,3 +71,8 @@
 }
 
 @end
+
+
+    
+
+    
